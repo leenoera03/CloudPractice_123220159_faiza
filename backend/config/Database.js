83d5fb763bac9ed604b6ece0946796913752fb6b
faizu <150577139+leenoera03@users.noreply.github.com>
2025-03-17 08:58:22 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,19 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize('task2_db', 'root','', {
-    host: 'localhost',
-    dialect: 'mysql',
-} );
+// Load file .env
+dotenv.config();
+
+const db = new Sequelize(
+  process.env.DB_NAME,   // Nama database
+  process.env.DB_USER,   // Username MySQL
+  process.env.DB_PASSWORD,  // Password MySQL
+  {
+    host: process.env.DB_HOST,   // Host MySQL
+    dialect: process.env.DB_DIALECT, // Dialect MySQL
+    port: process.env.DB_PORT, // Port MySQL
+  }
+);
 
 export default db;
+
