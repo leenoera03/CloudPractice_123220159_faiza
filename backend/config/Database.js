316dbd92502453576e88d75ede25a6a71337f693
leenoera03 <faizanurafida@gmail.com>
2025-03-18 22:21:09 +0700

    up3

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,33 +1,17 @@
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
 
-dotenv.config({ path: "./.env" });
-// Debugging: Pastikan env terbaca
-console.log("Loaded .env configuration:");
-console.log({
-  DB_HOST: process.env.DB_HOST,
-  DB_USER: process.env.DB_USER,
-  DB_PASSWORD: process.env.DB_PASSWORD ? "HIDDEN" : "NOT SET",
-  DB_NAME: process.env.DB_NAME,
-  DB_PORT: process.env.DB_PORT,
-  DB_DIALECT: process.env.DB_DIALECT
+const db = new Sequelize('task2_db', 'root', '1234', {
+    host: '35.238.144.225',
+    dialect: 'mysql',
+    port: 3306,
+    logging: false,  // Nonaktifkan logging query SQL
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
-const db = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT,
-    logging: false, 
-  }
-);
-
-// Cek koneksi ke database
-db.authenticate()
-  .then(() => console.log("Database connected..."))
-  .catch(err => console.error("Database connection error:", err));
-
 export default db;
+
