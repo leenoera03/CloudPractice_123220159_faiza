829a63f7250022490ae896849c0df368b230218b
faizu <150577139+leenoera03@users.noreply.github.com>
2025-03-21 05:54:05 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,17 +1,10 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('task2_db', 'root', '1234', {
-    host: '35.238.144.225',
+const db = new Sequelize('task2_db', 'root','1234', {
+    host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306,
-    logging: false,  // Nonaktifkan logging query SQL
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
+} );
 
 export default db;
 
